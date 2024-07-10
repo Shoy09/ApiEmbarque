95958a96d4ad6eac6e8d2f0ac0da340fc810f524
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-07-10 17:56:31 -0500

    jijiji

--- backend/urls.py
+++ backend/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('token/', obtain_tokens, name='token_obtain'),
     path('diarios-de-pesca/', DiarioDePescaListView.as_view(), name='diario_de_pesca_list'),
     path('diarios-de-pesca/<int:pk>/', DiarioDePescaDetailView.as_view(), name='diario_de_pesca_detail'),
-    path('api/diarios-de-pesca/<int:pk>/delete/', DiarioDePescaDeleteView.as_view(), name='diario_de_pesca_delete'),
+    path('diarios-de-pesca/<int:pk>/delete/', DiarioDePescaDeleteView.as_view(), name='diario_de_pesca_delete'),
     ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
