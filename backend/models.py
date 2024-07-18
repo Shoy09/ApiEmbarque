5f351c4dffc257d94103bec2c67c7cafd638c7d4
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-07-18 07:38:33 -0500

    miercoles

--- backend/models.py
+++ backend/models.py
@@ -87,16 +87,48 @@ class TipoDescripcion(models.Model):
 
 class CostoGalon(models.Model):
     fecha = models.DateField()
-    tipo = models.ForeignKey(TipoDescripcion,on_delete=models.CASCADE)
+    descripcion = models.ForeignKey(TipoDescripcion,on_delete=models.CASCADE)
     costo = models.DecimalField(max_digits=6, decimal_places=2)
 
 class FlotaDP(models.Model):
     fecha = models.DateField()
+    costo_galon = models.ForeignKey(CostoGalon,on_delete=models.CASCADE)
     embarcacion = models.ForeignKey(Embarcaciones,on_delete=models.CASCADE)
     zona_pesca = models.ForeignKey(ZonaPesca,on_delete=models.CASCADE)
     horas_faena = models.DurationField()
     kilos_declarados = models.DecimalField(max_digits=9, decimal_places=2)
-    especie = models.ForeignKey(Especies,on_delete=models.CASCADE)
- 
+    toneladas_procesadas= models.DecimalField(max_digits=9, decimal_places=2)
+    toneladas_recibidas = models.DecimalField(max_digits=9, decimal_places=2)
 
+    def __str__(self):
+        return f'{self.fecha} - {self.embarcacion} - {self.zona_pesca}'
 
+class FlotaDPDetalle(models.Model):
+    flota_dp = models.ForeignKey(FlotaDP, on_delete=models.CASCADE, related_name='detalles')
+    especie = models.ForeignKey(Especies, on_delete=models.CASCADE)
+    cantidad = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.flota_dp} - {self.especie} - {self.cantidad}'
+
+class CostoTripulacion(models.Model):
+    TarifasCostos = models.ForeignKey(TarifasCostos,on_delete=models.CASCADE)
+    participacion = models.DecimalField(max_digits=9, decimal_places=2)
+    bonificacion = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    total_participacion = models.DecimalField(max_digits=9, decimal_places=2)
+    aporte_REP= models.DecimalField(max_digits=9, decimal_places=2)
+    gratificacion = models.DecimalField(max_digits=9, decimal_places=2)
+    vacaciones = models.DecimalField(max_digits=9, decimal_places=2)
+    CTS = models.DecimalField(max_digits=9, decimal_places=2)
+    ESSALUD = models.DecimalField(max_digits=9, decimal_places=2)
+    SENATI = models.DecimalField(max_digits=9, decimal_places=2)
+    SCTR_SAL = models.DecimalField(max_digits=9, decimal_places=2)
+    SCTR_PEN = models.DecimalField(max_digits=9, decimal_places=2)
+    poliza_seguro = models.DecimalField(max_digits=9, decimal_places=2)
+    total_CT = models.DecimalField(max_digits=9, decimal_places=2)
+
+class Consumo(models.Model):
+    consumo_gasolina = models.DecimalField(max_digits=9, decimal_places=2)
+    consumo_hielo = models.DecimalField(max_digits=9, decimal_places=2)
+    consumo_agua = models.DecimalField(max_digits=9, decimal_places=2)
+    
